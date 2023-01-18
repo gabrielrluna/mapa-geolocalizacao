91d889900d565364db9cc0a5f7b62de36fba3b75
Gabriel Luna <lunagabri@gmail.com>
2023-01-18 11:56:35 -0300

    Update exercício

--- App.js
+++ App.js
@@ -1,40 +1,30 @@
-import { StyleSheet, Text, View, StatusBar } from "react-native";
+import { StyleSheet, Text, View, StatusBar, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
+import { useState } from "react";
 
 export default function App() {
-  const regiaoInicial = {
+  const [local, setLocal] = useState({
     latitude: -23.52618,
     longitude: -46.54027,
     latitudeDelta: 0.0222,
     longitudeDelta: 0.0121,
-  };
-  const localizacao = {
-    latitude: -33.867886,
-    longitude: -63.987,
-    latitudeDelta: 10,
-    longitudeDelta: 10,
-  };
+  });
+
   return (
     <>
       <StatusBar />
       <View style={estilos.container}>
         <MapView
           style={estilos.mapa}
-          initialRegion={localizacao}
+          initialRegion={local}
           liteMode={false} //Só android
           mapType="satellite" //Satellite, hybrid, standard
           userInterfaceStyle="dark"
           // maxZoomLevel={15} //Quanto maior o número, mais perto consigo buscar via zoom.
           // minZoomLevel={2} //Quanto maior o número, menos consigo tirar zoom.
+          onPress={(event) => event.nativeEvent(setLocal)}
         >
-          <Marker
-            draggable
-            coordinate={localizacao}
-            title={"Guitarrinha uhu"}
-            onPress={(event) => {
-              console.log(event.nativeEvent);
-            }}
-          />
+          <Marker draggable coordinate={local} title={"Guitarrinha uhu"} />
         </MapView>
       </View>
     </>
--- README.md
+++ README.md
@@ -3,4 +3,17 @@
 ## 01-api-mapview
 
 **Documentação:** https://docs.expo.dev/versions/latest/sdk/map-view/
-https://github.com/react-native-maps/react-native-maps
+
+## 02-api-mapview
+
+**Documentação:** https://github.com/react-native-maps/react-native-maps
+
+## 04-mapview-com-marcador-interativo
+
+**DESAFIO**
+
+Programe recursos que permitam colocar o `Marker` no local em que o usuário tocar no `Mapview`
+Dicas:
+
+- Você precisará trabalhar com `state` para registrar a localização, além de evento `onPress` e função.
+- Lembre-se que para acessar dados da localização, você pode utilizar as informações do `event.nativeEvent`
